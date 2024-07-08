d84fc7c7e9daa5bf6a0fd21c095c9633656b054f
Kingcedru <byishimocedrick@gmail.com>
2024-07-08 23:05:40 +0200

    created tasks array

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,16 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
+  imports: [FormsModule, CommonModule]
 })
 export class TodoListComponent implements OnInit {
+  taskArray = [{taskName: "Brush teeth", isCompleted: false}]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+
   }
 
 }
