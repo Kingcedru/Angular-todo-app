16652df71238e038d5bd2b06d653a88797de1369
Kingcedru <byishimocedrick@gmail.com>
2024-07-09 09:06:55 +0200

    disabled the button

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -17,11 +17,11 @@ export class TodoListComponent implements OnInit {
   ngOnInit() {}
 
   onSubmit(form: NgForm) {
-    console.log(form);
     this.taskArray.push({
       taskName: form.controls['task'].value,
       isCompleted: false,
     });
+    form.reset()
   }
 
   onDelete(index: number) {
